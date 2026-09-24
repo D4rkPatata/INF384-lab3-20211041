dbd14e4f9b64010946e3a78342d6d157f5c6d182
D4rkPatata <86811670+D4rkPatata@users.noreply.github.com>
2026-09-24 22:49:21 +0000

    inyeccion 1

---
+++ src/inyeccion1.js
@@ -0,0 +1,5 @@
+// INYECCION DE FALLA 1 - credencial falsa para probar la compuerta secretos
+const AWS_ACCESS_KEY_ID = 'AKIAZ3Q7XK2M4RTB4WHN';
+const AWS_SECRET_ACCESS_KEY = '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';
+
+module.exports = { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY };
